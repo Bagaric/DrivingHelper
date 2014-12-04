cc43d19a0614d3dbd3ca7d0d906cf2729f30a934
Cláudio Silva <2090283@C2-LCM-MAC01.local>
2014-12-04 17:23:59 +0000

    g-meter + settings

--- DrivingHelper/SecondViewController.swift
+++ DrivingHelper/SecondViewController.swift
@@ -19,14 +19,44 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var imgGBall: UIImageView!
     @IBOutlet weak var imgGBase: UIImageView!
-    
+    var rectangle:CGRect?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.imgGBase.layer.cornerRadius = self.imgGBase.bounds.size.width/2
+        rectangle = CGRect(origin: imgGBase.frame.origin, size: imgGBase.frame.size)
+        
+        // Check if the accelerometer is inactive and available
+        if self.motionManager.accelerometerActive {
+            self.stopAccelerometer()
+            return
+        }
+        if !self.motionManager.accelerometerAvailable {
+            println("No accelerometer detected.")
+            return
+        }
+        
+        // Run the accelerometer in the background
+        motionManager.accelerometerUpdateInterval = gmeterUpdateInterval
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: {(accelerometerData: CMAccelerometerData!, error:NSError!)in
+            self.outputAccelerationData(accelerometerData.acceleration)
+            if (error != nil) {
+                println("\(error)")
+            }
+        })
         
         
+    }
+    
+    func stopAccelerometer () {
+        self.motionManager.stopAccelerometerUpdates()
+    }
+    
+    
+    // Processes data taken from the accelerometer
+    func outputAccelerationData(acceleration:CMAcceleration) {
+        updateGmeter(CGFloat(acceleration.x*90), valueY: CGFloat(acceleration.z*90))
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,22 +65,28 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func btnMove(sender: AnyObject) {
-            var originX = self.imgGBase.frame.origin.x+90
-            var originY = self.imgGBase.frame.origin.y+90
-      
-            UIView.animateWithDuration(0.5, animations:{
-                self.imgGBall.frame.origin = CGPoint (x: originX+85, y: originY+30);
-            })
+        var originX = self.imgGBase.frame.origin.x+90
+        var originY = self.imgGBase.frame.origin.y+90
+        
+        UIView.animateWithDuration(0.5, animations:{
+            self.imgGBall.frame.origin = CGPoint (x: originX+85, y: originY+30);
+        })
     }
     
     func updateGmeter(valueX:CGFloat, valueY:CGFloat){
         var originX = self.imgGBase.frame.origin.x+90
         var originY = self.imgGBase.frame.origin.y+90
+        var newPoint = CGPoint (x: originX+valueX, y: originY+valueY)
         
-        UIView.animateWithDuration(gmeterUpdateInterval, animations:{
-            self.imgGBall.frame.origin = CGPoint (x: originX+valueX, y: originY+valueY);
-        })
-    
+        if !CGRectContainsPoint(rectangle!,newPoint){
+            var alert = UIAlertController(title: "Josip´s Fault", message: "Recent scientific research have proven that its Josip´s fault ^_^", preferredStyle: UIAlertControllerStyle.Alert)
+                        self.presentViewController(alert, animated: true, completion: nil)
+        }else{
+            UIView.animateWithDuration(gmeterUpdateInterval, animations:{
+                self.imgGBall.frame.origin = newPoint
+            })
+        }
+        
     }
 }
 
