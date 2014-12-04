6fee7dd708974b27f270a3ea110007df696a1d3d
Cláudio Silva <2090283@C2-LCM-MAC01.local>
2014-12-04 16:04:16 +0000

    Gmeter function

--- DrivingHelper/SecondViewController.swift
+++ DrivingHelper/SecondViewController.swift
@@ -14,24 +14,37 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var imgGBall: UIImageView!
     @IBOutlet weak var imgGBase: UIImageView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.imgGBase.layer.cornerRadius = self.imgGBase.bounds.size.width/2
+        
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func btnMove(sender: AnyObject) {
+            var originX = self.imgGBase.frame.origin.x+90
+            var originY = self.imgGBase.frame.origin.y+90
+      
+            UIView.animateWithDuration(0.5, animations:{
+                self.imgGBall.frame.origin = CGPoint (x: originX+85, y: originY+30);
+            })
+    }
+    
+    func updateGmeter(valorX:CGFloat,valorY:CGFloat){
+        var originX = self.imgGBase.frame.origin.x+90
+        var originY = self.imgGBase.frame.origin.y+90
         
-        
-        imgGBall.frame.origin = CGPoint (x: imgGBase.frame.origin.x+70, y: imgGBase.frame.origin.y + 70);
-        
+        UIView.animateWithDuration(0.5, animations:{
+            self.imgGBall.frame.origin = CGPoint (x: originX+valorX, y: originY+valorY);
+        })
+    
     }
     
 }
