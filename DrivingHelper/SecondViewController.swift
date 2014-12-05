ef64003bd8c91e05b3adfbce24603130e0e4cbc1
Josip Bagaric <baga@AsusMac.local>
2014-12-05 15:21:03 +0000

    stopwatch

--- DrivingHelper/SecondViewController.swift
+++ DrivingHelper/SecondViewController.swift
@@ -22,9 +22,18 @@ class SecondViewController: UIViewController {
     // Accelerometer initialization
     let motionManager = CMMotionManager()
     
+    
+    
     @IBOutlet weak var imgGBall: UIImageView!
     @IBOutlet weak var imgGBase: UIImageView!
     var rectangle:CGRect?
+    
+    @IBOutlet weak var stopwatch: UILabel!
+    @IBOutlet weak var lastLapTime: UILabel!
+    
+    var startTime = NSTimeInterval()
+    var timer: NSTimer = NSTimer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,11 +78,6 @@ class SecondViewController: UIViewController {
         updateGmeter((CGFloat(acceleration.x * 90) * limitGmeter), valueY: (CGFloat(acceleration.z * 90) * limitGmeter))
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func updateGmeter(valueX:CGFloat, valueY:CGFloat){
         var originX = self.imgGBase.frame.origin.x+90
         var originY = self.imgGBase.frame.origin.y+90
@@ -99,5 +103,54 @@ class SecondViewController: UIViewController {
         }
         
     }
+    
+    /*
+     *  Stopwatch stuff
+     */
+    
+    @IBAction func startStopwatch(sender: AnyObject) {
+        if (!timer.valid) {
+            let aSelector : Selector = "updateTime"
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+    }
+    
+    @IBAction func stopStopwatch(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    func updateTime() {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        let strMinutes = minutes > 9 ? String(minutes):"0" + String(minutes)
+        let strSeconds = seconds > 9 ? String(seconds):"0" + String(seconds)
+        let strFraction = fraction > 9 ? String(fraction):"0" + String(fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        stopwatch.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
 
