e2b6e956441e918594982c939585edea26085bd0
formando <formando@iMac-de-administrator.local>
2014-12-09 17:28:36 +0000

    List of history

--- DrivingHelper/HistoryViewController.swift
+++ DrivingHelper/HistoryViewController.swift
@@ -8,8 +8,46 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet
+    var tableView: UITableView!
     
+    var items: [String] = ["WeTESTE", "Heart", "Swift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "HistoryCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as CustomCell
+        
+        let hotelName = items[indexPath.row]
+        cell.lblTime.text = hotelName
+        cell.lblKMDone.text = hotelName
+        cell.lblSpeed.text = hotelName
+        cell.lblRating.text = hotelName
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+
     
 }
